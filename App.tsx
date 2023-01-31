71865b5c1f4aecec0c5ab3b3ca209ab362ae43af
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-01-31 22:12:17 +0900

    install react-native-video

--- App.tsx
+++ App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
+  StyleSheet,
 } from 'react-native';
+import Video from 'react-native-video';
 
 const App = () => {
   return (
-    <SafeAreaView />
+    <SafeAreaView>
+      <StatusBar />
+      <Video
+        source={''}
+        style={styles.backgroundVideo}
+      />
+    </SafeAreaView>
   );
 };
+
+// Later on in your styles..
+var styles = StyleSheet.create({
+  backgroundVideo: {
+    width: 300,
+    height: 300
+  },
+});
 
 export default App;
