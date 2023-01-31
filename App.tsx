71234efea461c55fb932202e7324c2987f158262
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-01-31 22:18:59 +0900

    implement only play video

--- App.tsx
+++ App.tsx
@@ -11,21 +11,49 @@ import styled from 'styled-components/native';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <>
+      <SafeAreaView />
       <StatusBar />
-      <Video
-        source={''}
-        style={styles.backgroundVideo}
-      />
-    </SafeAreaView>
+      <PlayerBackground>
+        <PlayerView>
+          <Video
+            source={require('./broadchurch.mp4')}
+            style={playerStyle}
+            resizeMode="contain"/>
+        </PlayerView>
+      </PlayerBackground>
+      <InfoArea>
+        <Text>テストテストテストテストテストテストテストテストテストテスト</Text>
+      </InfoArea>
+    </>
   );
 };
+
+export const playerStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+};
+
+const PlayerBackground = styled.View`
+`;
+
+const PlayerView = styled.View`
+  width: 100%;
+  aspectRatio: ${16 / 9};
+`;
+
+const InfoArea = styled.View`
+  padding: 12px 16px 0;
+`;
 
 // Later on in your styles..
 var styles = StyleSheet.create({
   backgroundVideo: {
-    width: 300,
-    height: 300
+    width: 100,
+    aspectRatio: 16/9,
   },
 });
 
