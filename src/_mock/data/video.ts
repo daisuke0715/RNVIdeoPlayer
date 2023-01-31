a9c9f825b06373d278a8206884cb7f963cda082c
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-01-31 22:56:33 +0900

    implement video chapter display logic

--- src/_mock/data/video.ts
+++ src/_mock/data/video.ts
@@ -1,38 +1,40 @@
-export const video = {
+import Videos from "types/Videos";
+
+export const video: Videos = {
   id: 6299869664001,
   title: 'タイトル',
   description: '説明',
-  thumbnail_url: require('./_assets/thumbnail1.png'),
+  thumbnail_url: require('../assets/thumbnail1.png'),
   is_recommended: true,
   total_duration_ms: 1000000,
   video_chapters: [
     {
       id: 6299869664001,
-      brightcove_video_external_id: 6299869664001,
+      source: require('../assets/video1.mp4'),
       description: '説明',
       duration_ms: '6299869664001',
-      thumbnail_url: require('../assets/thumbnail1.png'),
+      thumbnail: require('../assets/thumbnail1.png'),
     },
     {
       id: 6299869664001,
-      brightcove_video_external_id: 6299869664001,
+      source: require('../assets/video2.mp4'),
       description: '説明',
       duration_ms: '6299869664001',
-      thumbnail_url: require('../assets/thumbnail2.png'),
+      thumbnail: require('../assets/thumbnail2.png'),
     },
     {
       id: 6299869664001,
-      brightcove_video_external_id: 6299869664001,
+      source: require('../assets/video3.mp4'),
       description: '説明',
       duration_ms: '6299869664001',
-      thumbnail_url: require('../assets/thumbnail3.png'),
+      thumbnail: require('../assets/thumbnail3.png'),
     },
     {
       id: 6299869664001,
-      brightcove_video_external_id: 6299869664001,
+      source: require('../assets/video4.mp4'),
       description: '説明',
       duration_ms: '6299869664001',
-      thumbnail_url: require('../assets/thumbnail4.png'),
+      thumbnail: require('../assets/thumbnail4.png'),
     },
   ],
   related_company_ids: [6299869664001, 6299869664001, 6299869664001],
--- src/screen/video/VideoScreen.tsx
+++ src/screen/video/VideoScreen.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import IosVideoPlayer from 'react-native-video';
 // @ts-ignore
 import AndroidVideoPlayer from 'react-native-video-controls';
+import { PlayerBackground } from '../../components/PlayerBackground/style';
 import { PageWrapper } from '../../components/PageWrapper/style';
 import { PlayerView } from '../../components/PlayerView/style';
+import { InfoArea } from 'components/InfoArea/style';
+import Loading from '../../components/Loading';
+import VideoChapterType from '../../types/VideoChapter';
+import VideoChapter from '../../components/VideoChapter';
+import { video } from '../../_mock/data/video';
 
 const playerStyle = {
   position: 'absolute',
@@ -17,46 +23,80 @@ const playerStyle = {
 const VideoScreen: React.FC = () => {
   const isIos = Platform.OS === 'ios';
   const VideoPlayer = isIos ? IosVideoPlayer : AndroidVideoPlayer;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [stop, setStop] = useState<boolean>(false);
-  const [source, setSource] = useState<number>(require('../../_mock/assets/video1.mp4'));
+  const [source, setSource] = useState<number>();
+  const [chapters, setChapters] = useState<VideoChapterType[]>([]);
+  const [currentChapterIndex, setCurrentChapterIndex] = useState<number>(0);
   const [currentPlayer, setCurrentPlayer] = useState<string>('aa');
 
   const stopVideo = () => {
     setStop(true);
   };
 
-  const startVideo = () => {
-    setStop(false);
+  const getVideo = () => {
+    // NOTE: APIから取得したvideoChapterのデータをchaptersのstateにセットする処理を実装する
+    video.video_chapters[currentChapterIndex] && setSource(video.video_chapters[currentChapterIndex].source);
+    setIsLoading(false);
+  }
+
+  const changeVideoSource = (source: number, index: number) => {
+    setSource(source);
+    setCurrentChapterIndex(index);
+    setCurrentPlayer(currentPlayer + 'a');
   };
 
-  const changeVideoSource = () => {
-    stopVideo();
-    setSource(require('../../_mock/assets/video2.mp4'));
+  const playNextChapter = () => {
+    currentChapterIndex + 1 < video.video_chapters.length ? setCurrentChapterIndex((currentChapterIndex + 1)) : setCurrentChapterIndex(0);
     setCurrentPlayer(currentPlayer + 'a');
-    startVideo();
-  };
+  }
 
   useEffect(() => {
+    getVideo();
   }, []);
 
+  useEffect(() => {
+    video.video_chapters[currentChapterIndex] && setSource(video.video_chapters[currentChapterIndex].source);
+  }, [currentChapterIndex]);
+
+  if (isLoading) return <Loading />;
   return (
     <PageWrapper>
       { source && (
-        <PlayerView>
-          <VideoPlayer
-            key={currentPlayer}
-            source={source}
-            paused={stop}
-            controls={isIos}
-            resizeMode="contain"
-            playWhenInactive
-            pictureInPicture
-            maxBitRate={2000000}
-            minLoadRetryCount={5}
-            style={playerStyle}
-          />
-        </PlayerView>
+        <>
+          <PlayerBackground>
+            <PlayerView>
+              <VideoPlayer
+                key={currentPlayer}
+                source={source}
+                paused={stop}
+                controls={isIos}
+                resizeMode="contain"
+                playWhenInactive
+                pictureInPicture
+                maxBitRate={2000000}
+                minLoadRetryCount={5}
+                style={playerStyle}
+                onEnd={() => playNextChapter()}
+              />
+            </PlayerView>
+          </PlayerBackground>
+          <InfoArea>
+            <Text>{video.title}</Text>
+            <Text>{video.description}</Text>
+          </InfoArea>
+        </>
       )}
+      {
+        video.video_chapters.map((item, index) => (
+          <VideoChapter
+            currentPlayback={index === currentChapterIndex}
+            thumbnail={item.thumbnail}
+            description={item.description}
+            pressVideoChapter={() => changeVideoSource(item.source, index)}
+          />
+        ))
+      }
     </PageWrapper>
   );
 };
