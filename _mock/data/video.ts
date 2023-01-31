fec8dffea05570930a256cdb6434d67838597346
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-01-31 22:32:50 +0900

    prepare mock data

---
+++ _mock/data/video.ts
@@ -0,0 +1,40 @@
+export const video = {
+  id: 6299869664001,
+  title: 'タイトル',
+  description: '説明',
+  thumbnail_url: require('./_assets/thumbnail1.png'),
+  is_recommended: true,
+  total_duration_ms: 1000000,
+  video_chapters: [
+    {
+      id: 6299869664001,
+      brightcove_video_external_id: 6299869664001,
+      description: '説明',
+      duration_ms: '6299869664001',
+      thumbnail_url: require('../assets/thumbnail1.png'),
+    },
+    {
+      id: 6299869664001,
+      brightcove_video_external_id: 6299869664001,
+      description: '説明',
+      duration_ms: '6299869664001',
+      thumbnail_url: require('../assets/thumbnail2.png'),
+    },
+    {
+      id: 6299869664001,
+      brightcove_video_external_id: 6299869664001,
+      description: '説明',
+      duration_ms: '6299869664001',
+      thumbnail_url: require('../assets/thumbnail3.png'),
+    },
+    {
+      id: 6299869664001,
+      brightcove_video_external_id: 6299869664001,
+      description: '説明',
+      duration_ms: '6299869664001',
+      thumbnail_url: require('../assets/thumbnail4.png'),
+    },
+  ],
+  related_company_ids: [6299869664001, 6299869664001, 6299869664001],
+  related_video_bundle_ids: [6299869664001, 6299869664001, 6299869664001],
+};
---
+++ _mock/data/videos.ts
@@ -0,0 +1,13 @@
+export const videos = {
+  video_bundles: [
+    {
+      id: 6299869664001,
+      title: 'タイトル',
+      description: '説明',
+      thumbnail_url: require('../assets/thumbnail1.jpeg'),
+      is_recommended: true,
+      total_duration_ms: '123456789',
+    },
+  ],
+  total_count: 1,
+}
